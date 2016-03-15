09cabf42f9c47dfe7cc3f8f21e5231f8a0e2daa1
abby-garcia <acg361@gmail.com>
2016-03-15 12:41:49 -0400

    added some js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
 
+
+// showInfo function - this will show the information
+//getInfo function - this will get the information
+$(function(){
+  $('#search-term').submit(function(event){
+    event.preventDefault();
+    var searchTerm = $('#query').val();
+    getRequest(searchTerm);
+  });
+});
+
+
+function getInfo(query){
+	var params ={
+		id: // what do I put there?
+	};
+	url ="https://api.spotify.com/v1/artists/" + id + "related-artists";
+
+	$.getJSON(url, params, function(data){
+		console.log(data);
+    	showResults(data.artists);
+  	});
+
+
+};
+
+function showInfo(results){
+	var html ="";
+	$.each(results,function(){
+		console.log(  artists.url)
+
+	}
+
+}
