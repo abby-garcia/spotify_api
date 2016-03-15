fdcaaf9fb8d7f96d8bbf715b3bf61402f2d81a08
abby-garcia <acg361@gmail.com>
2016-03-15 16:54:12 -0400

    api

--- js/main.js
+++ js/main.js
@@ -6,30 +6,34 @@ $(function(){
   $('#search-term').submit(function(event){
     event.preventDefault();
     var searchTerm = $('#query').val();
-    getRequest(searchTerm);
+    getInfo(query);
   });
 });
+
+// we need to get the id! 
 
 
 function getInfo(query){
 	var params ={
-		id: // what do I put there?
+		id: 'artists.id'
 	};
 	url ="https://api.spotify.com/v1/artists/" + id + "related-artists";
 
 	$.getJSON(url, params, function(data){
 		console.log(data);
-    	showResults(data.artists);
+    	showInfo(data.artists);
   	});
-
-
-};
+}
 
 function showInfo(results){
 	var html ="";
-	$.each(results,function(){
-		console.log(  artists.url)
-
-	}
-
+	$.each(results,function(value){
+		console.log(value.artists.url);
+		html += '<p>' + value.artists + '</p>';
+ });
+  $('#newArtists').html(html);
 }
+
+// for class  artists.name
+// for picture artists.images.url
+// for link 
