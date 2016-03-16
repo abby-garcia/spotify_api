75c4b6cbddf74dd7ea9f51147736b904ee1ead08
abby-garcia <acg361@gmail.com>
2016-03-16 00:32:15 -0400

    api finishing touches

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(function(){
   $('#search-term').submit(function(event){
     event.preventDefault();
     var searchTerm = $('#query').val();
-    getInfo(query);
+    getInfo(searchTerm);
   });
 });
 
@@ -15,23 +15,52 @@ $(function(){
 
 function getInfo(query){
 	var params ={
-		id: 'artists.id'
+		q: query,
+		type: "artist"
 	};
-	url ="https://api.spotify.com/v1/artists/" + id + "related-artists";
+
+	url ="https://api.spotify.com/v1/search";
+
 
 	$.getJSON(url, params, function(data){
-		console.log(data);
-    	showInfo(data.artists);
+		var id = data.artists.items[0].id;
+		var name = data.artists.items[0].name;
+		var image = data.artists.items[0].images[0].url;
+		console.log(id);
+		console.log(name);
+		// console.log(image);
+		getRelatedArtist(id);
+
   	});
+
 }
 
-function showInfo(results){
+function getRelatedArtist(id){
+
+	url ="https://api.spotify.com/v1/artists/" + id + "/related-artists";
+
+	$.getJSON(url, function(data){
+		showInfo(data.artists);
+    	// showInfo(data.artists);
+  	});
+
+};
+
+function showInfo(results){ // I think i'm the most stuck here
 	var html ="";
-	$.each(results,function(value){
-		console.log(value.artists.url);
-		html += '<p>' + value.artists + '</p>';
+	console.log(results);
+	$.each(results,function(key, value){
+		var artist_name = results[key].name;
+		var artist_picture = results[key].images[0].url;
+		var artist_link = results[key].external_urls.spotify;
+
+	    html += '<ul class="result"><li class="name">' + artist_name + '</li>';
+	    html += '<li class="picture"><img src="' + artist_picture + '" width="100px"/></li>';
+	    html += '<li class="link"><a href="' + artist_link + '">Listen Now</a></li></ul>';
+	    
  });
-  $('#newArtists').html(html);
+	 // console.log($('.newArtists .result'));
+  $('.newArtists').append(html);
 }
 
 // for class  artists.name
